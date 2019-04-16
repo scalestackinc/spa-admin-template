0f2c7b9e4abc73453172353517b3b7339587ef46
Nasir dowlatkhahi <nasir@scalestack.io>
2019-04-15 23:39:10 -0400

    Login page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import { Admin, Resource } from 'react-admin';
 import { UserList } from './Users';
 import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
+import { Login } from './layout';
+import { Dashboard } from './dashboard';
 
-import logo from './logo.svg';
 import './App.css';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => <Admin 
-  dataProvider={dataProvider}>
+  dataProvider={dataProvider}
+  dashboard={Dashboard}
+  loginPage={Login}
+  authProvider={authProvider}>
     <Resource name="users" list={UserList} />
   </Admin>;
 
